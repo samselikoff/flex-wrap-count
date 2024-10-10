f4fc98bd6b437c59e43a5038bdd582aae8cf72dc
Sam Selikoff <sam.selikoff@gmail.com>
2024-10-10 01:56:11 -0400

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,63 @@
-import Image from "next/image";
+'use client';
+import { RefObject, useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  let ref = useRef<HTMLDivElement>(null);
+  let [overflowCount, setOverflowCount] = useState(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useOnResize(ref, (entry) => {
+    let offsets = Array.from(entry.target.children).map((el) =>
+      el instanceof HTMLElement ? el.offsetTop : 0
+    );
+    let firstNumber = offsets[0];
+    let count = offsets.findIndex((num) => num !== firstNumber);
+    let result = count === -1 ? offsets.length : count;
+
+    setOverflowCount(offsets.length - result);
+  });
+
+  return (
+    <div className="mx-auto mt-8">
+      <div className="flex items-center px-4">
+        <div ref={ref} className="flex h-8 overflow-hidden gap-4 flex-wrap">
+          {Array.from(Array(20).keys()).map((i) => (
+            <div key={i} className="size-8 bg-green-500" />
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="whitespace-nowrap pl-2">+ {overflowCount}</div>
+      </div>
+
+      <div className="mt-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+          consequuntur debitis est eligendi neque iure quis tenetur corrupti et?
+          Molestias ullam quis enim dolore nisi praesentium doloremque nobis
+          numquam laborum!
+        </p>
+      </div>
     </div>
   );
+}
+
+function useOnResize<T extends Element>(
+  ref: RefObject<T>,
+  callback: (entry: ResizeObserverEntry) => void
+) {
+  useEffect(() => {
+    let el = ref.current;
+    if (!el) return;
+
+    let observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        callback(entry);
+      }
+    });
+
+    observer.observe(el);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, callback]);
 }
